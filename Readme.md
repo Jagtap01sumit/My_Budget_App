1bf65dfa7bc52eac0c70dc299fadd4ab0cd3bece
Sumit <jagtasumit668@gmail.com>
2024-05-03 13:15:29 +0530

    installation and setup

---
+++ Readme.md
@@ -0,0 +1,9 @@
+## Installation :
+
+### https://docs.expo.dev/router/installation/
+
+## Installation of AsyncStorage (local storage)
+
+### https://docs.expo.dev/versions/latest/sdk/async-storage/
+
+### https://react-native-async-storage.github.io/async-storage/docs/usage/
---
+++ app/index.jsx
@@ -0,0 +1,13 @@
+import { Button, StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { Link } from "expo-router";
+
+export default function Home() {
+  return (
+    <View style={{ margin: 10 }}>
+      <Text style={{ margin: 10 }}>Home</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
